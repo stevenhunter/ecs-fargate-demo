d4706a52dde0294d6ca06e5109ddc13f047d7d84
Steven Hunter <stevenxhunter@gmail.com>
2022-11-04 15:24:27 +0000

    updated response and added request logging

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(JSON.stringify({ message: 'NodeJS App Running on Amazon ECS Fargate' }));
+    res.send(JSON.stringify({ message: 'NodeJS App with source code in Github, deployed by AWS CodePipeline to ECS Fargate!!!' }));
+
+    console.log(`Request received from ${req.ip}`);
 })
 
 app.listen(3000, () => {
